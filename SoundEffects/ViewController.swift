0dd11e94458b7a556554fa269941980233e0f9b6
Pawarit_Bunrith <pawarit.b@Pawarit-Bunriths-MacBook-Pro.local>
2015-06-05 23:13:42 +0700

    Initial Commit

---
+++ SoundEffects/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundEffects
+//
+//  Created by Pawarit_Bunrith on 6/5/2558 BE.
+//  Copyright (c) 2558 Pawarit_Bunrith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
